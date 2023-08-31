f803ca7c60db636a0e937b42578e6176495f752a
coderalberto <asacil.austria@gmail.com>
2023-08-31 17:33:32 +0200

    Task 3

---
+++ MyStack/MyStack/MyStackBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    public abstract class MyStackBase : IMyStack<int>
+    {
+        protected int count;
+        public int Count { get { return count; } }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(int item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ MyStack/MyStackTest/MyStackBaseTest.cs
@@ -0,0 +1,20 @@
+﻿using MyStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStackTest
+{
+    [TestClass]
+    public class MyStackBaseTest : MyStackBase
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            MyStackBaseTest myStackBaseTest = new MyStackBaseTest();
+            Assert.AreEqual(myStackBaseTest.Count, 0);
+        }
+    }
+}
